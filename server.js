849ece78e1bf207cc461fcd807d961481073010b
Bruce <brucegraham11@gmail.com>
2017-01-23 16:56:09 -0800

    Another deployment try for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var overRide = require('method-override');
 var morgan = require('morgan');
 
 app.use(express.static('./'));
